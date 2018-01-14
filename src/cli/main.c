4b9ac9977b48bd6f8dbf39f0786a89ad2a7bcb1d
Filip Stanis <filip@stanis.me>
2018-01-14 18:06:14 +0000

    polished up CLI

--- src/cli/main.c
+++ src/cli/main.c
@@ -2,13 +2,24 @@
 // This code is licensed under MIT license (see LICENSE for details)
 
 #include <stdio.h>
+#include <string.h>
 
 #include "setbackground.h"
+#include "errors.h"
 
 int main(int argc, char** argv) {
-  if (argc != 2) {
-    fprintf(stderr, "must specify 1 argument, got %d\n", argc-1);
+  if ((argc != 2) || (strcmp(argv[1], "--help") == 0)) {
+    puts(
+      "setbackground is a cross-platform tool for changing the desktop "
+      "background (wallpaper).\n\n"
+      "Usage: setbackground [file]\n");
     return 1;
   }
-  printf("%d\n", set_background(argv[1]));
+  int error = set_background(argv[1]);
+  if (error != E_SUCCESS) {
+    fprintf(stderr, "An error occurred while attemping to change the background: error code %d\n", error);
+    return 1;
+  }
+  puts("Background image changed.");
+  return 0;
 }
