0536b0fc612eda64d2101416ca33d796f554fe7d
Filip Stanis <filip@stanis.me>
2018-04-02 22:36:06 +0100

    Make MacOS background change apply to all screens

--- src/lib/darwin/core.m
+++ src/lib/darwin/core.m
@@ -22,26 +22,42 @@ int set_background_darwin(const char* path, int style) {
       return E_UNSUPPORTED_STYLE;
     }
 
-    NSURL* image = [NSURL fileURLWithPath:[NSString stringWithUTF8String:path] isDirectory:NO];
+    NSURL* image = [NSURL fileURLWithPath:
+      [NSString stringWithUTF8String:path]
+      isDirectory:NO];
     NSError* err;
     if ([image checkResourceIsReachableAndReturnError:&err] == NO) {
       return E_INVALID_PATH;
     }
     NSWorkspace* sw = [NSWorkspace sharedWorkspace];
-    NSScreen* screen = [NSScreen mainScreen];
 
-    NSMutableDictionary *opts = [[sw desktopImageOptionsForScreen:screen] mutableCopy];
-    if (style == STYLE_CENTER) {
-      [opts setObject:[NSNumber numberWithInt:NSImageScaleNone] forKey:NSWorkspaceDesktopImageScalingKey];
-      [opts setObject:[NSNumber numberWithBool:NO] forKey:NSWorkspaceDesktopImageAllowClippingKey];
-    } else if (style == STYLE_STRETCH) {
-      [opts setObject:[NSNumber numberWithInt:NSImageScaleAxesIndependently] forKey:NSWorkspaceDesktopImageScalingKey];
-      [opts setObject:[NSNumber numberWithBool:YES] forKey:NSWorkspaceDesktopImageAllowClippingKey];
-    }
+    NSArray* screens = [NSScreen screens];
+    for (NSScreen* screen in screens) {
+      NSMutableDictionary* opts = [[sw desktopImageOptionsForScreen:screen]
+        mutableCopy];
+      if (style == STYLE_CENTER) {
+        [opts setObject:
+          [NSNumber numberWithInt:NSImageScaleNone]
+          forKey:NSWorkspaceDesktopImageScalingKey];
+        [opts setObject:
+          [NSNumber numberWithBool:NO]
+          forKey:NSWorkspaceDesktopImageAllowClippingKey];
+      } else if (style == STYLE_STRETCH) {
+        [opts setObject:
+          [NSNumber numberWithInt:NSImageScaleAxesIndependently]
+          forKey:NSWorkspaceDesktopImageScalingKey];
+        [opts setObject:
+          [NSNumber numberWithBool:YES]
+          forKey:NSWorkspaceDesktopImageAllowClippingKey];
+      }
 
-    bool success = [sw setDesktopImageURL:image forScreen:screen options:opts error:&err];
-    if (!success) {
-      return E_FAIL;
+      bool success = [sw setDesktopImageURL:
+        image forScreen:screen
+        options:opts
+        error:&err];
+      if (!success) {
+        return E_FAIL;
+      }
     }
     return E_SUCCESS;
   }
